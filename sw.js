a27e7e52a88e59c61f128ca1d7f73bc78cf7ab17
khepri-ux <khenril2006@gmail.com>
2026-07-28 00:57:13 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'diario-missoes-v1';
+const ARQUIVOS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ARQUIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((chaves) =>
+      Promise.all(chaves.filter((c) => c !== CACHE).map((c) => caches.delete(c)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
